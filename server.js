e97f872c313ef594f97982083cd59ed55f24ae75
Jackbennett <jbennett@birkdalehigh.co.uk>
2016-05-11 15:55:39 +0100

    Print the hostname of the computer when running

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
+var os = require('os')
 
 app.use( express.static(__dirname) )
 
 http.listen(3000, '0.0.0.0', function(){
-  console.log(`listening on ${http.address().address} at: ${http.address().port}`)
+  console.log(`listening on ${ os.hostname() } at: ${http.address().port}`)
 })
